cbd23f16c25bce840c0b181cfefba108d7634207
yanaTKD <71845191+yanaTKD@users.noreply.github.com>
2020-10-08 17:49:50 +0200

    Update lab_exd2.py

--- lab_exd2.py
+++ lab_exd2.py
@@ -3,10 +3,10 @@ b = int(input())
 c = int(input())
 if a + b > c and a + c > b and b + c > a:
     print('Треугольник существует')
-    if a == b or a == c or c == b:
-        print('Треугольник равносторонний')
-    elif a == b and b == c:
+    if (a == b and a != c) or (a == c and a != b) or (c == b and c != a):
         print('Треугольник равнобедренный')
+    elif a == b and b == c:
+        print('Треугольник равносторонний')
     else:
         print('Треугольник разносторонний')
 else:
